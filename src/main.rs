354ac23a75737f965bce1a06bb68c18d1b0b8a62
guimcaballero <guim.caballero@gmail.com>
2020-11-05 21:53:14 +0100

    Add camera and plane

--- src/main.rs
+++ src/main.rs
@@ -12,5 +12,34 @@ fn main() {
             ..Default::default()
         })
         .add_plugins(DefaultPlugins)
+        .add_startup_system(setup.system())
         .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands
+        // Plane
+        .spawn(PbrComponents {
+            mesh: meshes.add(Mesh::from(shape::Plane { size: 8.0 })),
+            material: materials.add(Color::rgb(1., 0.9, 0.9).into()),
+            transform: Transform::from_translation(Vec3::new(4., 0., 4.)),
+            ..Default::default()
+        })
+        // Camera
+        .spawn(Camera3dComponents {
+            transform: Transform::from_matrix(Mat4::from_rotation_translation(
+                Quat::from_xyzw(-0.3, -0.5, -0.3, 0.5).normalize(),
+                Vec3::new(-7.0, 20.0, 4.0),
+            )),
+            ..Default::default()
+        })
+        // Light
+        .spawn(LightComponents {
+            transform: Transform::from_translation(Vec3::new(4.0, 8.0, 4.0)),
+            ..Default::default()
+        });
 }
