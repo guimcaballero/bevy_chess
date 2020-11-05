e158ad59dc8184b1bfc1fbcf44e747cf9b768cc8
guimcaballero <guim.caballero@gmail.com>
2020-11-05 14:21:18 +0100

    Change window settings

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::build().add_plugins(DefaultPlugins).run();
+    App::build()
+        // Set antialiasing to use 4 samples
+        .add_resource(Msaa { samples: 4 })
+        // Set WindowDescriptor Resource to change title and size
+        .add_resource(WindowDescriptor {
+            title: "Chess!".to_string(),
+            width: 1600,
+            height: 1600,
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
+        .run();
 }
