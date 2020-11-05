7739d027e4fae8dc13e2ca1776e0e62b3b5dd5ee
guimcaballero <guim.caballero@gmail.com>
2020-11-05 22:36:45 +0100

    Game board

--- src/main.rs
+++ src/main.rs
@@ -13,22 +13,12 @@ fn main() {
         })
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup.system())
+        .add_startup_system(create_board.system())
         .run();
 }
 
-fn setup(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-) {
+fn setup(mut commands: Commands) {
     commands
-        // Plane
-        .spawn(PbrComponents {
-            mesh: meshes.add(Mesh::from(shape::Plane { size: 8.0 })),
-            material: materials.add(Color::rgb(1., 0.9, 0.9).into()),
-            transform: Transform::from_translation(Vec3::new(4., 0., 4.)),
-            ..Default::default()
-        })
         // Camera
         .spawn(Camera3dComponents {
             transform: Transform::from_matrix(Mat4::from_rotation_translation(
@@ -42,4 +32,32 @@ fn setup(
             transform: Transform::from_translation(Vec3::new(4.0, 8.0, 4.0)),
             ..Default::default()
         });
+}
+
+fn create_board(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // Add meshes and materials
+    let mesh = meshes.add(Mesh::from(shape::Plane { size: 1. }));
+    let white_material = materials.add(Color::rgb(1., 0.9, 0.9).into());
+    let black_material = materials.add(Color::rgb(0., 0.1, 0.1).into());
+
+    // Spawn 64 squares
+    for i in 0..8 {
+        for j in 0..8 {
+            commands.spawn(PbrComponents {
+                mesh: mesh.clone(),
+                // Change material according to position to get alternating pattern
+                material: if (i + j + 1) % 2 == 0 {
+                    white_material.clone()
+                } else {
+                    black_material.clone()
+                },
+                transform: Transform::from_translation(Vec3::new(i as f32, 0., j as f32)),
+                ..Default::default()
+            });
+        }
+    }
 }
