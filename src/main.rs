1cdcbb8ac708937a243a418d6eb64b419cc6517c
guimcaballero <guim.caballero@gmail.com>
2020-11-06 18:16:04 +0100

    Adding pieces

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use bevy::prelude::*;
 
+mod pieces;
+use pieces::*;
+
 fn main() {
     App::build()
         // Set antialiasing to use 4 samples
@@ -14,6 +17,7 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup.system())
         .add_startup_system(create_board.system())
+        .add_startup_system(create_pieces.system())
         .run();
 }
 
@@ -59,5 +63,155 @@ fn create_board(
                 ..Default::default()
             });
         }
+    }
+}
+
+fn create_pieces(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // Load all the meshes
+    let king_handle: Handle<Mesh> =
+        asset_server.load("models/chess_kit/pieces.glb#Mesh0/Primitive0");
+    let king_cross_handle: Handle<Mesh> =
+        asset_server.load("models/chess_kit/pieces.glb#Mesh1/Primitive0");
+    let pawn_handle: Handle<Mesh> =
+        asset_server.load("models/chess_kit/pieces.glb#Mesh2/Primitive0");
+    let knight_1_handle: Handle<Mesh> =
+        asset_server.load("models/chess_kit/pieces.glb#Mesh3/Primitive0");
+    let knight_2_handle: Handle<Mesh> =
+        asset_server.load("models/chess_kit/pieces.glb#Mesh4/Primitive0");
+    let rook_handle: Handle<Mesh> =
+        asset_server.load("models/chess_kit/pieces.glb#Mesh5/Primitive0");
+    let bishop_handle: Handle<Mesh> =
+        asset_server.load("models/chess_kit/pieces.glb#Mesh6/Primitive0");
+    let queen_handle: Handle<Mesh> =
+        asset_server.load("models/chess_kit/pieces.glb#Mesh7/Primitive0");
+
+    // Add some materials
+    let white_material = materials.add(Color::rgb(1., 0.8, 0.8).into());
+    let black_material = materials.add(Color::rgb(0.3, 0.3, 0.3).into());
+
+    spawn_rook(
+        &mut commands,
+        white_material.clone(),
+        rook_handle.clone(),
+        Vec3::new(0., 0., 0.),
+    );
+    spawn_knight(
+        &mut commands,
+        white_material.clone(),
+        knight_1_handle.clone(),
+        knight_2_handle.clone(),
+        Vec3::new(0., 0., 1.),
+    );
+    spawn_bishop(
+        &mut commands,
+        white_material.clone(),
+        bishop_handle.clone(),
+        Vec3::new(0., 0., 2.),
+    );
+    spawn_queen(
+        &mut commands,
+        white_material.clone(),
+        queen_handle.clone(),
+        Vec3::new(0., 0., 3.),
+    );
+    spawn_king(
+        &mut commands,
+        white_material.clone(),
+        king_handle.clone(),
+        king_cross_handle.clone(),
+        Vec3::new(0., 0., 4.),
+    );
+    spawn_bishop(
+        &mut commands,
+        white_material.clone(),
+        bishop_handle.clone(),
+        Vec3::new(0., 0., 5.),
+    );
+    spawn_knight(
+        &mut commands,
+        white_material.clone(),
+        knight_1_handle.clone(),
+        knight_2_handle.clone(),
+        Vec3::new(0., 0., 6.),
+    );
+    spawn_rook(
+        &mut commands,
+        white_material.clone(),
+        rook_handle.clone(),
+        Vec3::new(0., 0., 7.),
+    );
+
+    for i in 0..8 {
+        spawn_pawn(
+            &mut commands,
+            white_material.clone(),
+            pawn_handle.clone(),
+            Vec3::new(1., 0., i as f32),
+        );
+    }
+
+    spawn_rook(
+        &mut commands,
+        black_material.clone(),
+        rook_handle.clone(),
+        Vec3::new(7., 0., 0.),
+    );
+    spawn_knight(
+        &mut commands,
+        black_material.clone(),
+        knight_1_handle.clone(),
+        knight_2_handle.clone(),
+        Vec3::new(7., 0., 1.),
+    );
+    spawn_bishop(
+        &mut commands,
+        black_material.clone(),
+        bishop_handle.clone(),
+        Vec3::new(7., 0., 2.),
+    );
+    spawn_queen(
+        &mut commands,
+        black_material.clone(),
+        queen_handle.clone(),
+        Vec3::new(7., 0., 3.),
+    );
+    spawn_king(
+        &mut commands,
+        black_material.clone(),
+        king_handle.clone(),
+        king_cross_handle.clone(),
+        Vec3::new(7., 0., 4.),
+    );
+    spawn_bishop(
+        &mut commands,
+        black_material.clone(),
+        bishop_handle.clone(),
+        Vec3::new(7., 0., 5.),
+    );
+    spawn_knight(
+        &mut commands,
+        black_material.clone(),
+        knight_1_handle.clone(),
+        knight_2_handle.clone(),
+        Vec3::new(7., 0., 6.),
+    );
+    spawn_rook(
+        &mut commands,
+        black_material.clone(),
+        rook_handle.clone(),
+        Vec3::new(7., 0., 7.),
+    );
+
+    for i in 0..8 {
+        spawn_pawn(
+            &mut commands,
+            black_material.clone(),
+            pawn_handle.clone(),
+            Vec3::new(6., 0., i as f32),
+        );
     }
 }
---
+++ src/pieces.rs
@@ -0,0 +1,177 @@
+use bevy::prelude::*;
+
+pub fn spawn_king(
+    commands: &mut Commands,
+    material: Handle<StandardMaterial>,
+    mesh: Handle<Mesh>,
+    mesh_cross: Handle<Mesh>,
+    position: Vec3,
+) {
+    commands
+        // Spawn parent entity
+        .spawn(PbrComponents {
+            transform: Transform::from_translation(position),
+            ..Default::default()
+        })
+        // Add children to the parent
+        .with_children(|parent| {
+            parent.spawn(PbrComponents {
+                mesh,
+                material: material.clone(),
+                transform: {
+                    let mut transform = Transform::from_translation(Vec3::new(-0.2, 0., -1.9));
+                    transform.apply_non_uniform_scale(Vec3::new(0.2, 0.2, 0.2));
+                    transform
+                },
+                ..Default::default()
+            });
+            parent.spawn(PbrComponents {
+                mesh: mesh_cross,
+                material,
+                transform: {
+                    let mut transform = Transform::from_translation(Vec3::new(-0.2, 0., -1.9));
+                    transform.apply_non_uniform_scale(Vec3::new(0.2, 0.2, 0.2));
+                    transform
+                },
+                ..Default::default()
+            });
+        });
+}
+
+pub fn spawn_knight(
+    commands: &mut Commands,
+    material: Handle<StandardMaterial>,
+    mesh_1: Handle<Mesh>,
+    mesh_2: Handle<Mesh>,
+    position: Vec3,
+) {
+    commands
+        // Spawn parent entity
+        .spawn(PbrComponents {
+            transform: Transform::from_translation(position),
+            ..Default::default()
+        })
+        // Add children to the parent
+        .with_children(|parent| {
+            parent.spawn(PbrComponents {
+                mesh: mesh_1,
+                material: material.clone(),
+                transform: {
+                    let mut transform = Transform::from_translation(Vec3::new(-0.2, 0., 0.9));
+                    transform.apply_non_uniform_scale(Vec3::new(0.2, 0.2, 0.2));
+                    transform
+                },
+                ..Default::default()
+            });
+            parent.spawn(PbrComponents {
+                mesh: mesh_2,
+                material,
+                transform: {
+                    let mut transform = Transform::from_translation(Vec3::new(-0.2, 0., 0.9));
+                    transform.apply_non_uniform_scale(Vec3::new(0.2, 0.2, 0.2));
+                    transform
+                },
+                ..Default::default()
+            });
+        });
+}
+
+pub fn spawn_queen(
+    commands: &mut Commands,
+    material: Handle<StandardMaterial>,
+    mesh: Handle<Mesh>,
+    position: Vec3,
+) {
+    commands
+        .spawn(PbrComponents {
+            transform: Transform::from_translation(position),
+            ..Default::default()
+        })
+        .with_children(|parent| {
+            parent.spawn(PbrComponents {
+                mesh,
+                material,
+                transform: {
+                    let mut transform = Transform::from_translation(Vec3::new(-0.2, 0., -0.95));
+                    transform.apply_non_uniform_scale(Vec3::new(0.2, 0.2, 0.2));
+                    transform
+                },
+                ..Default::default()
+            });
+        });
+}
+
+pub fn spawn_bishop(
+    commands: &mut Commands,
+    material: Handle<StandardMaterial>,
+    mesh: Handle<Mesh>,
+    position: Vec3,
+) {
+    commands
+        .spawn(PbrComponents {
+            transform: Transform::from_translation(position),
+            ..Default::default()
+        })
+        .with_children(|parent| {
+            parent.spawn(PbrComponents {
+                mesh,
+                material,
+                transform: {
+                    let mut transform = Transform::from_translation(Vec3::new(-0.1, 0., 0.));
+                    transform.apply_non_uniform_scale(Vec3::new(0.2, 0.2, 0.2));
+                    transform
+                },
+                ..Default::default()
+            });
+        });
+}
+
+pub fn spawn_rook(
+    commands: &mut Commands,
+    material: Handle<StandardMaterial>,
+    mesh: Handle<Mesh>,
+    position: Vec3,
+) {
+    commands
+        .spawn(PbrComponents {
+            transform: Transform::from_translation(position),
+            ..Default::default()
+        })
+        .with_children(|parent| {
+            parent.spawn(PbrComponents {
+                mesh,
+                material,
+                transform: {
+                    let mut transform = Transform::from_translation(Vec3::new(-0.1, 0., 1.8));
+                    transform.apply_non_uniform_scale(Vec3::new(0.2, 0.2, 0.2));
+                    transform
+                },
+                ..Default::default()
+            });
+        });
+}
+
+pub fn spawn_pawn(
+    commands: &mut Commands,
+    material: Handle<StandardMaterial>,
+    mesh: Handle<Mesh>,
+    position: Vec3,
+) {
+    commands
+        .spawn(PbrComponents {
+            transform: Transform::from_translation(position),
+            ..Default::default()
+        })
+        .with_children(|parent| {
+            parent.spawn(PbrComponents {
+                mesh,
+                material,
+                transform: {
+                    let mut transform = Transform::from_translation(Vec3::new(-0.2, 0., 2.6));
+                    transform.apply_non_uniform_scale(Vec3::new(0.2, 0.2, 0.2));
+                    transform
+                },
+                ..Default::default()
+            });
+        });
+}
