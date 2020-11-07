6805cc61c8c5d1a0375634b3896e7359546839bf
guimcaballero <guim.caballero@gmail.com>
2020-11-07 12:34:43 +0100

    Refactor

---
+++ src/board.rs
@@ -0,0 +1,32 @@
+use bevy::prelude::*;
+use bevy_mod_picking::*;
+
+fn create_board(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // Add meshes and materials
+    let mesh = meshes.add(Mesh::from(shape::Plane { size: 1. }));
+    let white_material = materials.add(Color::rgb(1., 0.9, 0.9).into());
+    let black_material = materials.add(Color::rgb(0., 0.1, 0.1).into());
+
+    // Spawn 64 squares
+    for i in 0..8 {
+        for j in 0..8 {
+            commands
+                .spawn(PbrComponents {
+                    mesh: mesh.clone(),
+                    // Change material according to position to get alternating pattern
+                    material: if (i + j + 1) % 2 == 0 {
+                        white_material.clone()
+                    } else {
+                        black_material.clone()
+                    },
+                    transform: Transform::from_translation(Vec3::new(i as f32, 0., j as f32)),
+                    ..Default::default()
+                })
+                .with(PickableMesh::default());
+        }
+    }
+}
--- src/pieces.rs
+++ src/pieces.rs
@@ -1,6 +1,156 @@
 use bevy::prelude::*;
 
-pub fn spawn_king(
+pub fn create_pieces(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // Load all the meshes
+    let king_handle: Handle<Mesh> =
+        asset_server.load("models/chess_kit/pieces.glb#Mesh0/Primitive0");
+    let king_cross_handle: Handle<Mesh> =
+        asset_server.load("models/chess_kit/pieces.glb#Mesh1/Primitive0");
+    let pawn_handle: Handle<Mesh> =
+        asset_server.load("models/chess_kit/pieces.glb#Mesh2/Primitive0");
+    let knight_1_handle: Handle<Mesh> =
+        asset_server.load("models/chess_kit/pieces.glb#Mesh3/Primitive0");
+    let knight_2_handle: Handle<Mesh> =
+        asset_server.load("models/chess_kit/pieces.glb#Mesh4/Primitive0");
+    let rook_handle: Handle<Mesh> =
+        asset_server.load("models/chess_kit/pieces.glb#Mesh5/Primitive0");
+    let bishop_handle: Handle<Mesh> =
+        asset_server.load("models/chess_kit/pieces.glb#Mesh6/Primitive0");
+    let queen_handle: Handle<Mesh> =
+        asset_server.load("models/chess_kit/pieces.glb#Mesh7/Primitive0");
+
+    // Add some materials
+    let white_material = materials.add(Color::rgb(1., 0.8, 0.8).into());
+    let black_material = materials.add(Color::rgb(0.3, 0.3, 0.3).into());
+
+    spawn_rook(
+        &mut commands,
+        white_material.clone(),
+        rook_handle.clone(),
+        Vec3::new(0., 0., 0.),
+    );
+    spawn_knight(
+        &mut commands,
+        white_material.clone(),
+        knight_1_handle.clone(),
+        knight_2_handle.clone(),
+        Vec3::new(0., 0., 1.),
+    );
+    spawn_bishop(
+        &mut commands,
+        white_material.clone(),
+        bishop_handle.clone(),
+        Vec3::new(0., 0., 2.),
+    );
+    spawn_queen(
+        &mut commands,
+        white_material.clone(),
+        queen_handle.clone(),
+        Vec3::new(0., 0., 3.),
+    );
+    spawn_king(
+        &mut commands,
+        white_material.clone(),
+        king_handle.clone(),
+        king_cross_handle.clone(),
+        Vec3::new(0., 0., 4.),
+    );
+    spawn_bishop(
+        &mut commands,
+        white_material.clone(),
+        bishop_handle.clone(),
+        Vec3::new(0., 0., 5.),
+    );
+    spawn_knight(
+        &mut commands,
+        white_material.clone(),
+        knight_1_handle.clone(),
+        knight_2_handle.clone(),
+        Vec3::new(0., 0., 6.),
+    );
+    spawn_rook(
+        &mut commands,
+        white_material.clone(),
+        rook_handle.clone(),
+        Vec3::new(0., 0., 7.),
+    );
+
+    for i in 0..8 {
+        spawn_pawn(
+            &mut commands,
+            white_material.clone(),
+            pawn_handle.clone(),
+            Vec3::new(1., 0., i as f32),
+        );
+    }
+
+    spawn_rook(
+        &mut commands,
+        black_material.clone(),
+        rook_handle.clone(),
+        Vec3::new(7., 0., 0.),
+    );
+    spawn_knight(
+        &mut commands,
+        black_material.clone(),
+        knight_1_handle.clone(),
+        knight_2_handle.clone(),
+        Vec3::new(7., 0., 1.),
+    );
+    spawn_bishop(
+        &mut commands,
+        black_material.clone(),
+        bishop_handle.clone(),
+        Vec3::new(7., 0., 2.),
+    );
+    spawn_queen(
+        &mut commands,
+        black_material.clone(),
+        queen_handle.clone(),
+        Vec3::new(7., 0., 3.),
+    );
+    spawn_king(
+        &mut commands,
+        black_material.clone(),
+        king_handle.clone(),
+        king_cross_handle.clone(),
+        Vec3::new(7., 0., 4.),
+    );
+    spawn_bishop(
+        &mut commands,
+        black_material.clone(),
+        bishop_handle.clone(),
+        Vec3::new(7., 0., 5.),
+    );
+    spawn_knight(
+        &mut commands,
+        black_material.clone(),
+        knight_1_handle.clone(),
+        knight_2_handle.clone(),
+        Vec3::new(7., 0., 6.),
+    );
+    spawn_rook(
+        &mut commands,
+        black_material.clone(),
+        rook_handle.clone(),
+        Vec3::new(7., 0., 7.),
+    );
+
+    for i in 0..8 {
+        spawn_pawn(
+            &mut commands,
+            black_material.clone(),
+            pawn_handle.clone(),
+            Vec3::new(6., 0., i as f32),
+        );
+    }
+}
+
+fn spawn_king(
     commands: &mut Commands,
     material: Handle<StandardMaterial>,
     mesh: Handle<Mesh>,
@@ -38,7 +188,7 @@ pub fn spawn_king(
         });
 }
 
-pub fn spawn_knight(
+fn spawn_knight(
     commands: &mut Commands,
     material: Handle<StandardMaterial>,
     mesh_1: Handle<Mesh>,
@@ -76,7 +226,7 @@ pub fn spawn_knight(
         });
 }
 
-pub fn spawn_queen(
+fn spawn_queen(
     commands: &mut Commands,
     material: Handle<StandardMaterial>,
     mesh: Handle<Mesh>,
@@ -101,7 +251,7 @@ pub fn spawn_queen(
         });
 }
 
-pub fn spawn_bishop(
+fn spawn_bishop(
     commands: &mut Commands,
     material: Handle<StandardMaterial>,
     mesh: Handle<Mesh>,
@@ -126,7 +276,7 @@ pub fn spawn_bishop(
         });
 }
 
-pub fn spawn_rook(
+fn spawn_rook(
     commands: &mut Commands,
     material: Handle<StandardMaterial>,
     mesh: Handle<Mesh>,
@@ -151,7 +301,7 @@ pub fn spawn_rook(
         });
 }
 
-pub fn spawn_pawn(
+fn spawn_pawn(
     commands: &mut Commands,
     material: Handle<StandardMaterial>,
     mesh: Handle<Mesh>,
